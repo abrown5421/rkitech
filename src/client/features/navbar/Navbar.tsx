2c64a0814fb2778dd25aaf2897857ba92e913abb
abrown5421 <augustashtonbrown@gmail.com>
2025-06-16 14:10:34 -0400

    sprucing up the navbar a bit to be ready for user customization in Admin

--- src/client/features/navbar/Navbar.tsx
+++ src/client/features/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import type { NavbarProps } from './navbarTypes';
 
-const Navbar: React.FC = () => {
+const Navbar: React.FC<NavbarProps> = ({ twClasses = [] }) => {
 
      return (
-         <div className='flex flex-row justify-between shadow-md'>
+         <div className={`flex flex-row justify-between shadow-md ${twClasses.join(' ')}`}>
             <div>logo section</div>
             <div>menu section</div>
          </div>
