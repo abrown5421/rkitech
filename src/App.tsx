51e994ba3f76cc0eb6088ead7d49f4a00d3b4760
abrown5421 <augustashtonbrown@gmail.com>
2025-07-14 14:29:02 -0400

    Started building a modal feature for displaying modals overtop of the page content. TODO: type the feature further and allow for custom entrance and exit animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import Loader from './shared/components/loader/Loader';
+import Button from './shared/components/button/Button';
+import { openModal } from './features/modal/modalSlice';
+import { useDispatch } from 'react-redux';
+import Modal from './features/modal/Modal';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(openModal({
+      title: 'Confirm Action',
+      content: <div>Are you sure you want to proceed?</div>,
+    }));
+  }
 
   return (
-    <div className='w-screen'>
-      <Loader variant="flash" color="bg-blue-500" size={12} />
-      <Loader variant="bounce" color="bg-red-400" size={20} />
-      <Loader variant="spinner" color="border-green-500" size={30} />
+    <div className='w-screen h-screen z-30 relative'>
+      <Button onClick={handleClick} color="success">open modal</Button>
+      <Modal />
     </div>
+    
   );
 };
 
