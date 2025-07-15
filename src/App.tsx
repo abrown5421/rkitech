d8f47f11bac11f52fffd3263e73abaf46f95b318
abrown5421 <augustashtonbrown@gmail.com>
2025-07-15 10:36:19 -0400

    adding in a logo to the navbar for branding purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import Button from './shared/components/button/Button';
 import Modal from './features/modal/Modal';
-import { useAppDispatch } from './app/hooks';
 import Alert from './features/alert/Alert';
-import { openDrawer } from './features/drawer/drawerSlice';
 import Drawer from './features/drawer/Drawer';
 import Navbar from './features/navbar/Navbar';
 
 const App: React.FC = () => {
-  const dispatch = useAppDispatch();
-  
-  const handleClick = () => {
-    dispatch(openDrawer({
-      drawerOpen: true,
-      drawertitle: 'Drawer Content',
-      draweranchor: 'right',
-      draweranimation: {
-        entranceAnimation: 'animate__fadeInRight animate__faster',
-        exitAnimation: 'animate__fadeOutRight animate__faster',
-        isEntering: true,
-      },
-      drawerchildren: (
-        <div>
-          <p>This can be anything you want inside the drawer.</p>
-        </div>
-      ),
-    }));
-  };
 
   return (
     <div className='w-screen h-screen z-30 relative'>
--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Container from '../../shared/components/container/Container';
 import Text from '../../shared/components/text/Text';
+import Button from '../../shared/components/button/Button';
+import Image from '../../shared/components/image/Image';
 
 const Navbar: React.FC = () => {
 
      return (
          <Container height={50} padding='sm' justifyContent='between' alignItems='center' className='shadow-[0_2px_4px_rgba(0,0,0,0.05)]'>
-            <Text text='Rkitech' bold={true} size='xl' font='primary' />
-            <Text text='menu' />
+            <Container alignItems='center'>
+                <Image src="../../../public/assets/images/logo.png" height={50} alt='Rkitech' />
+                <Text text='Rkitech' bold={true} size='xl' font='primary' />
+            </Container>
+            <Button padding='sm' color='primary' cursor='pointer'>
+                <Text text='Login' color='white' />
+            </Button>
          </Container>
      );
 };
