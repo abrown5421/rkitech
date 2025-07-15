d5e280c523d1a289dc43bd38da5eaee89f5b6ee9
abrown5421 <augustashtonbrown@gmail.com>
2025-07-15 11:21:36 -0400

    creating a page shell feature. This page shell for now just assigns a blanket paddign class and a calculated height to account for the navbar. In the future we will type this for props that will determine color, dynamic vs static page rendering and other things

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Modal from './features/modal/Modal';
 import Alert from './features/alert/Alert';
 import Drawer from './features/drawer/Drawer';
 import Navbar from './features/navbar/Navbar';
+import PageShell from './features/pageShell/PageShell';
 
 const App: React.FC = () => {
 
   return (
     <div className='w-screen h-screen z-30 relative bg-black'>
       <Navbar />
+      <PageShell />
       <Modal />
       <Alert />
       <Drawer />
