9bf323509212035ac08b2a919a912bba7510c661
abrown5421 <augustashtonbrown@gmail.com>
2025-07-15 13:28:51 -0400

    completing the page shell animation transition handlers and tying them into the activePageShellIn state property so we can seamlessly transition between pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,35 @@ import Drawer from './features/drawer/Drawer';
 import Navbar from './features/navbar/Navbar';
 import PageShell from './features/pageShell/PageShell';
 import { Route, Routes } from 'react-router-dom';
+import { useAppSelector } from './app/hooks';
 
 const App: React.FC = () => {
+  const activePage = useAppSelector((state) => state.pageShell);
 
   return (
     <div className='w-screen h-screen z-30 relative bg-black'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<PageShell activePageShellBgColor='bg-white' />} />
-        <Route path='/Login' element={<PageShell activePageShellBgColor='bg-transparent' />} />
+        <Route path='/' element={
+          <PageShell 
+            activePageShellBgColor='bg-white' 
+            activePageShellAnimation = {{
+              entranceAnimation: 'animate__fadeIn',
+              exitAnimation: 'animate__fadeOut',
+              isEntering: activePage.activePageShellIn,
+            }}
+          />
+        } />
+        <Route path='/Login' element={
+          <PageShell 
+            activePageShellBgColor='bg-transparent'
+            activePageShellAnimation = {{
+              entranceAnimation: 'animate__fadeInUpBig',
+              exitAnimation: 'animate__fadeOutDownBig',
+              isEntering: activePage.activePageShellIn,
+            }}
+          />
+        } />
       </Routes>
       <Modal />
       <Alert />
