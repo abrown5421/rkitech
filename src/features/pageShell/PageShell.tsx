8727360ede7d16e7944e08c45b8dab5eafa8f693
abrown5421 <augustashtonbrown@gmail.com>
2025-07-15 11:34:59 -0400

    adding typed bg color, animations, and render method to the page shell feature

--- src/features/pageShell/PageShell.tsx
+++ src/features/pageShell/PageShell.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '../../shared/components/container/Container';
+import type { PageShellState } from './pageShellTypes';
 
-const PageShell: React.FC = () => {
+const PageShell: React.FC<PageShellState> = ({
+    pageShellRenderMethod = 'dynamic',
+    pageShellBackgroundColor = 'bg-white',
+    pageShellAnimation = {
+        entranceAnimation: 'animate__fadeIn',
+        exitAnimation: 'animate__fadeOut',
+        isEntering: true,
+    }
+}) => {
+
+    useEffect(()=>{console.log(pageShellRenderMethod)}, [])
 
     return (
-        <Container padding='md' className='bg-white z-20 h-[calc(100vh-50px)]' >
+        <Container 
+            padding='md' 
+            className={`${pageShellBackgroundColor} z-20 h-[calc(100vh-50px)]`}
+            animation={{
+                entranceExit: {
+                    entranceAnimation: pageShellAnimation.entranceAnimation,
+                    exitAnimation: pageShellAnimation.exitAnimation,
+                    isEntering: pageShellAnimation.isEntering,
+                },
+            }}
+        >
             page
         </Container>
     );
