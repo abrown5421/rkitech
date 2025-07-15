ce851e0956e7c8efdab3d7701b5502d7e4c1d830
abrown5421 <augustashtonbrown@gmail.com>
2025-07-15 11:55:39 -0400

    forgot to add the new slice to the store so I imported it so we can use it throughout the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Modal from './features/modal/Modal';
 import Alert from './features/alert/Alert';
 import Drawer from './features/drawer/Drawer';
 import Navbar from './features/navbar/Navbar';
 import PageShell from './features/pageShell/PageShell';
+import { useAppSelector } from './app/hooks';
 
 const App: React.FC = () => {
+  const activePage = useAppSelector((state) => state.pageShell);
 
+  useEffect(()=>{console.log(activePage)}, [activePage])
   return (
     <div className='w-screen h-screen z-30 relative bg-black'>
       <Navbar />
