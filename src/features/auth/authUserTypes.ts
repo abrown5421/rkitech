aab6ef4bdb0c83b3970e604f7313d30c5d1ad5ff
august5421 <70552491+august5421@users.noreply.github.com>
2025-07-16 15:45:33 -0400

    new authUserTypes.ts and authUserSlice.ts files for storing user data on login and signup

---
+++ src/features/auth/authUserTypes.ts
@@ -0,0 +1,12 @@
+export interface AuthUser {
+    userId: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    userRole: 'User' | 'Admin' | string;
+    createdAt: string; 
+}
+
+export interface AuthUserState {
+    user: AuthUser | null;
+}
