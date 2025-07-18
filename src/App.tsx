a09cea805f09f781316e72b30d98853509e08bc5
abrown5421 <augustashtonbrown@gmail.com>
2025-07-18 17:54:58 -0400

    adding in the pages array but importing it from the db so that it will work in tandem with our cms

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,28 @@ import { useAppDispatch, useAppSelector } from './app/hooks';
 import { useNavigationHook } from './hooks/useNavigationHook';
 import Cookies from 'js-cookie';
 import { setAuthUser } from './features/auth/authUserSlice';
+import { useInitializeApp } from './hooks/useInitializeApp';
+import Loader from './shared/components/loader/Loader';
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
   const clientNavigation = useNavigationHook();
+  const loadingSite = useInitializeApp();
   const activePage = useAppSelector((state) => state.pageShell);
-  const authUser = useAppSelector((state) => state.authUser);
+  const pages = useAppSelector((state) => state.pages.pages);
 
-   useEffect(() => {
-        const storedUser = Cookies.get('authUser');
-        if (storedUser) {
-            try {
-                const parsedUser = JSON.parse(storedUser);
-                dispatch(setAuthUser(parsedUser));
-            } catch (e) {
-                console.error('Failed to parse user from cookie', e);
-                Cookies.remove('authUser'); 
-            }
+  useEffect(() => {
+    const storedUser = Cookies.get('authUser');
+    if (storedUser) {
+        try {
+            const parsedUser = JSON.parse(storedUser);
+            dispatch(setAuthUser(parsedUser));
+        } catch (e) {
+            console.error('Failed to parse user from cookie', e);
+            Cookies.remove('authUser'); 
         }
-    }, [dispatch]);
-
-  const pages = [
-    {pageName: 'Home', pageId:'homePage', pagePath: '/', pageBg: 'bg-white', pageEntranceAnimation: 'animate__fadeIn', pageExitAnimation: 'animate__fadeOut'},
-    {pageName: 'Auth', pageId:'authenticationPage', pagePath: '/login', pageBg: 'bg-transparent', pageEntranceAnimation: 'animate__fadeInUpBig', pageExitAnimation: 'animate__fadeOutDownBig'},
-    {pageName: 'Auth', pageId:'authenticationPage', pagePath: '/sign-up', pageBg: 'bg-transparent', pageEntranceAnimation: 'animate__fadeInUpBig', pageExitAnimation: 'animate__fadeOutDownBig'},
-  ];
+    }
+  }, [dispatch]);
 
   useEffect(()=>{
     const getPage = pages.find((page) => page.pagePath === location.pathname);
@@ -43,30 +40,38 @@ const App: React.FC = () => {
   }, [])
 
   return (
-    <div className='w-screen h-screen z-30 relative bg-black'>
-      <Navbar />
-      <Routes>
-        {pages.map((page) => { 
-          return (
-            <Route path={page.pagePath} element={
-                <PageShell 
-                  activePageShellBgColor={page.pageBg} 
-                  activePageShellAnimation = {{
-                    entranceAnimation: page.pageEntranceAnimation,
-                    exitAnimation: page.pageExitAnimation,
-                    isEntering: activePage.activePageShellIn,
-                  }}
-                />
-              } 
-            />
-          )
-        })}
-      </Routes>
-      <Modal />
-      <Alert />
-      <Drawer />
-    </div>
     
+    <>  
+      {!loadingSite ? (
+        <div className='w-screen h-screen z-30 relative bg-black'>
+          <Navbar />
+          <Routes>
+            {pages.map((page) => { 
+              return (
+                <Route path={page.pagePath} element={
+                    <PageShell 
+                      activePageShellBgColor={page.pageBg} 
+                      activePageShellAnimation = {{
+                        entranceAnimation: page.pageEntranceAnimation,
+                        exitAnimation: page.pageExitAnimation,
+                        isEntering: activePage.activePageShellIn,
+                      }}
+                    />
+                  } 
+                />
+              )
+            })}
+          </Routes>
+          <Modal />
+          <Alert />
+          <Drawer />
+        </div>
+      ) : (
+        <div className='w-screen h-screen z-30 relative bg-black flex justify-center items-center'>
+          <Loader variant='bounce' color='bg-primary' />
+        </div>
+      )}
+    </>    
   );
 };
 
