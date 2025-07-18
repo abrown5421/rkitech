dd0177612412a1d6778d3b50c372b3505ea96737
abrown5421 <augustashtonbrown@gmail.com>
2025-07-18 17:21:27 -0400

    adding in an initialization hook so we can load all of the necessaries on app start

---
+++ src/hooks/useInitializeApp.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useAppDispatch } from "../app/hooks";
+import { listenToCollection } from "../services/database/listenForData";
+import { setPages } from "../features/pages/pagesSlice";
+
+export const useInitializeApp = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unsubscribers: (() => void)[] = [];
+    
+    unsubscribers.push(
+      listenToCollection("Pages", (data) => {
+        const pagesWithDocId = data.map(({ id, ...rest }) => ({
+          pageID: id,
+          ...rest,
+        }));
+        dispatch(setPages(pagesWithDocId));
+        setLoading(false); 
+      })
+    );
+
+    return () => {
+      unsubscribers.forEach((unsubscribe) => unsubscribe());
+    };
+  }, [dispatch]);
+
+  return loading;
+};
