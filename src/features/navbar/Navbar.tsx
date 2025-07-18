802d500ab007e148e619ad43f55330fb922538fe
abrown5421 <augustashtonbrown@gmail.com>
2025-07-17 20:02:34 -0400

    opening a drawer with the avatar in the navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -4,9 +4,13 @@ import Text from '../../shared/components/text/Text';
 import Button from '../../shared/components/button/Button';
 import Image from '../../shared/components/image/Image';
 import { useNavigationHook } from '../../hooks/useNavigationHook';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { openDrawer } from '../drawer/drawerSlice';
 
 const Navbar: React.FC = () => {
+    const dispatch = useAppDispatch();
     const clientNavigation = useNavigationHook();
+    const authUser = useAppSelector((state) => state.authUser);
 
      return (
          <Container height={50} padding='sm' justifyContent='between' alignItems='center' bgColor='bg-white' className='relative z-40 shadow-[0_2px_4px_rgba(0,0,0,0.05)]'>
@@ -54,16 +58,39 @@ const Navbar: React.FC = () => {
                 > 
                     <Text text='Test Page' color='text-black' />
                 </Button>
-                <Button 
-                    padding='sm' 
-                    color='primary' 
-                    cursor='pointer'
-                    onClick={() =>
-                        clientNavigation('/login', 'Auth', 'authenticationPage')()
-                    }
-                > 
-                    <Text text='Login' color='white' />
-                </Button>
+                {authUser?.user ? (
+                    <Image
+                        src="../../../public/assets/images/placeholder-avatar.png" 
+                        alt="User Avatar"
+                        width={40}
+                        height={40}
+                        className="rounded-full border border-gray-300 cursor-pointer"
+                        onClick={() => 
+                            dispatch(openDrawer({
+                                drawerOpen: true,
+                                drawertitle: 'Good Evening',
+                                draweranchor: 'right',
+                                drawerchildren: <div>stuff</div>,
+                                draweranimation: {
+                                    entranceAnimation: 'animate__fadeInRight animate__faster',
+                                    exitAnimation: 'animate__fadeOutRight animate__faster',
+                                    isEntering: true,
+                                }
+                            }))
+                        }
+                    />
+                ) : (
+                    <Button
+                        padding="sm"
+                        color="primary"
+                        cursor="pointer"
+                        onClick={() =>
+                            clientNavigation('/login', 'Auth', 'authenticationPage')()
+                        }
+                    >
+                        <Text text="Login" color="white" />
+                    </Button>
+                )}
             </Container>
          </Container>
      );
