2b1067179de0e35ddd83149c5522a4dee6523af0
august5421 <70552491+august5421@users.noreply.github.com>
2025-07-22 16:45:11 -0400

    converting the profile page to utilize the container component

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Container from '../../shared/components/container/Container';
 
 const Profile: React.FC = () => {
 
     return (
-        <div className=''>
+        <Container padding='sm'>
             Profile
-        </div>
+        </Container>
     );
 };
 
