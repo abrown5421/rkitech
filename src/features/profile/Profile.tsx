091492493c21a8efc6942a91a4e7443a7deb9715
abrown5421 <augustashtonbrown@gmail.com>
2025-07-23 16:09:40 -0400

    adding in a bit of flair to the profile page. for now most of it is just placeholder until we can get some of the mechanics up and working. Before we do that I have to make some modifications to the modal and drawer ui components as well as add a couple of parsing utilities that will help us display the actual data for the user

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,13 +1,134 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '../../shared/components/container/Container';
+import { useParams } from 'react-router-dom';
+import { getDocumentById } from '../../services/database/readData';
+import type { AuthUser } from '../auth/authUserTypes';
+import Loader from '../../shared/components/loader/Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { setLoading, setNotLoading } from '../../app/globalSlices/loading/loadingSlice';
+import Text from '../../shared/components/text/Text';
+import Image from '../../shared/components/image/Image';
 
 const Profile: React.FC = () => {
+  const { userId } = useParams();
+  const dispatch = useAppDispatch();
+  const { loading, id } = useAppSelector((state) => state.loading);
+  const isLoading = loading && id === 'profile';
 
-    return (
-        <Container padding='sm'>
-            Profile
+  const [profileUser, setProfileUser] = useState<AuthUser | null>(null);
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      if (!userId) return;
+
+      try {
+        dispatch(setLoading({ loading: true, id: 'profile' }));
+
+        const data = await getDocumentById('Users', userId);
+        if (data) {
+          setProfileUser(data as AuthUser);
+        }
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      } finally {
+        dispatch(setNotLoading());
+      }
+    };
+
+    fetchProfileData();
+  }, [userId, dispatch]);
+
+  return (
+    <Container height="h-full" width="w-full" flexDirection="col">
+      {isLoading ? (
+        <Loader variant="spinner" color="bg-primary" />
+      ) : profileUser ? (
+        <>
+          {/* Banner */}
+          <Image
+            src="../../../public/assets/images/placeholder-banner.png"
+            alt="User Banner"
+            width="w-full"
+            height={300}
+          />
+
+          <Container
+            flexDirection="row"
+            className="relative"
+          >
+            <Container
+              flexDirection="col"
+              className="flex-[3] relative min-w-[240px]"
+            >
+              <Container
+                padding='xl'
+                className="absolute top-0 transform -translate-y-1/2 z-10"
+              >
+                {profileUser?.profileImage ? (
+                  <Image
+                    src={profileUser.profileImage}
+                    alt="User Avatar"
+                    width={160}
+                    height={160}
+                    className="rounded-full border-4 border-white shadow-lg"
+                  />
+                ) : (
+                  <Container
+                    width={160}
+                    height={160}
+                    className="rounded-full bg-black cursor-pointer flex justify-center items-center border-4 border-white shadow-lg"
+                  >
+                    <Text
+                      color="text-white"
+                      font="primary"
+                      size="6x"
+                      className="w-full flex justify-center items-center"
+                      text={`${profileUser.firstName?.[0] || ''}${profileUser.lastName?.[0] || ''}`.toUpperCase()}
+                    />
+                  </Container>
+                )}
+              </Container>
+
+              <div style={{ height: 80 }} />
+
+              <Container flexDirection="col" padding='xl'>
+                <Text
+                  color="text-black"
+                  size="2x"
+                  bold
+                  text={profileUser.firstName + ' ' + profileUser.lastName}
+                />
+                <Text color="text-black" size="xl" text={profileUser.email} />
+                <Text
+                  text={`Member since: ${profileUser.createdAt}`}
+                  size="sm"
+                  color="text-gray-500"
+                />
+              </Container>
+            </Container>
+
+            <Container flexDirection="col" className="flex-[9]" padding='xl'>
+              profile stuff
+            </Container>
+          </Container>
+        </>
+      ) : (
+        <Container
+          flexDirection="col"
+          height="h-full"
+          width="w-full"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text
+            text="We are sorry there is no profile with that user ID."
+            size="xl"
+            color="text-black"
+          />
         </Container>
-    );
+      )}
+    </Container>
+  );
 };
 
 export default Profile;
