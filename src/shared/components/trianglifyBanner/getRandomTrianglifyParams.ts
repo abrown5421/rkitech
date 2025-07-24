a050fbc640ebc35ec7f88c2b4eb8a760d3728e18
abrown5421 <augustashtonbrown@gmail.com>
2025-07-24 12:13:55 -0400

    modifying the randomizing function so that it produces our x and y colors in tupples

--- src/shared/components/trianglifyBanner/getRandomTrianglifyParams.ts
+++ src/shared/components/trianglifyBanner/getRandomTrianglifyParams.ts
@@ -1,14 +1,25 @@
-export function getRandomTrianglifyParams() {
-  function getRandomHexColor() {
+export function getRandomTrianglifyParams(): {
+  xColor: [string, string];
+  yColor: [string, string];
+  variance: number;
+  cellSize: number;
+} {
+  function getRandomHexColor(): string {
     const hex = Math.floor(Math.random() * 0xffffff).toString(16);
     return '#' + hex.padStart(6, '0').toUpperCase();
   }
-  function getRandomColorArray() {
+
+  function getRandomColorTuple(): [string, string] {
     return [getRandomHexColor(), getRandomHexColor()];
   }
+
   const variance = +(Math.random() * (1 - 0.01) + 0.01).toFixed(2);
   const cellSize = Math.floor(Math.random() * (100 - 20 + 1)) + 20;
-  const xColor = getRandomColorArray();
-  const yColor = getRandomColorArray();
-  return { xColor, yColor, variance, cellSize };
+
+  return {
+    xColor: getRandomColorTuple(),
+    yColor: getRandomColorTuple(),
+    variance,
+    cellSize,
+  };
 }
