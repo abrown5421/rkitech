f2f1c91aa41af589aaf78dd4e640aa879bcdd98d
abrown5421 <augustashtonbrown@gmail.com>
2025-07-25 15:14:35 -0400

    modifying text so that it works with both strings and arrays

--- src/shared/text/Text.tsx
+++ src/shared/text/Text.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import type { TextProps } from './textTypes'
+import type { TextProps } from './textTypes';
 
-const Text: React.FC<TextProps> = ({ text, twClasses = [], ...rest }) => {
+const Text: React.FC<TextProps> = ({ text, twClasses, ...rest }) => {
+  const classString = Array.isArray(twClasses) ? twClasses.join(' ') : twClasses || '';
+
   return (
-    <div className={`component-root ${twClasses.join(' ')}`} {...rest}>
+    <div className={`component-root ${classString}`} {...rest}>
       {text}
     </div>
   );
