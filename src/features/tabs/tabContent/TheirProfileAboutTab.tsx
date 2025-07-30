66d54b271fc8b8d4c6e305afcea3b51aebd7d0ff
abrown5421 <augustashtonbrown@gmail.com>
2025-07-30 16:30:08 -0400

    simplifying the their profile tab to just include info not in the sidebar

--- src/features/tabs/tabContent/TheirProfileAboutTab.tsx
+++ src/features/tabs/tabContent/TheirProfileAboutTab.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import Container from "../../../shared/components/container/Container";
 import Text from "../../../shared/components/text/Text";
-import Image from "../../../shared/components/image/Image";
 import Icon from "../../../shared/components/icon/Icon";
-import { format } from "date-fns";
 import type { ProfileTab } from "../tabTypes";
 
 export const TheirProfileAboutTab: React.FC<ProfileTab> = ({
   profileUser,
 }) => {
-  const fullName = `${profileUser.firstName?.charAt(0).toUpperCase() || ""}${
-    profileUser.firstName?.slice(1) || ""
-  } ${profileUser.lastName?.charAt(0).toUpperCase() || ""}${
-    profileUser.lastName?.slice(1) || ""
-  }`;
-
-  const initials = `${profileUser.firstName?.[0] || ""}${
-    profileUser.lastName?.[0] || ""
-  }`.toUpperCase();
 
   const fullAddress = profileUser.addressLn1 &&
     profileUser.addressCity &&
@@ -27,45 +16,8 @@ export const TheirProfileAboutTab: React.FC<ProfileTab> = ({
     : "No address available";
 
   return (
-    <>
-      <Container TwClassName="flex-col md:flex-row">
-        <Container TwClassName="flex-col flex-2 items-center md:items-start">
-          {profileUser?.profileImage ? (
-            <Image
-              src={profileUser.profileImage}
-              alt="User Avatar"
-              width={100}
-              height={100}
-              TwClassName="rounded-full border-4 border-white shadow-lg object-cover"
-            />
-          ) : (
-            <Container TwClassName="w-[100px] h-[100px] rounded-full bg-black cursor-pointer flex justify-center items-center border-4 border-white shadow-lg">
-              <Text
-                TwClassName="text-white font-primary text-4xl w-full flex justify-center items-center"
-                text={initials}
-              />
-            </Container>
-          )}
-        </Container>
-        <Container TwClassName="flex-col flex-9 justify-center items-center md:items-start">
-          <Text TwClassName="text-black text-xl font-bold" text={fullName} />
-          {profileUser.bio && (
-            <Text TwClassName="text-black text-md" text={profileUser.bio} />
-          )}
-          <Text
-            text={`Member since: ${format(profileUser.createdAt, "EEEE, MMMM do, yyyy")}`}
-            TwClassName="text-xs text-gray-500"
-          />
-        </Container>
-      </Container>
-
-      <Container TwClassName="flex-row mt-5">
-        <Container TwClassName="hidden md:block flex-2 gap-5 w-full">
-          <Container TwClassName="w-[100px] h-[100px] rounded-full bg-black cursor-pointer flex justify-center items-center border-4 border-white shadow-lg invisible">
-            <span></span>
-          </Container>
-        </Container>
-        <Container TwClassName="flex-col flex-9 gap-5 w-full items-center md:items-start">
+      <Container TwClassName="flex-row">
+        <Container TwClassName="flex-col flex-9 gap-5 w-full">
           <Text TwClassName="text-black text-xl font-bold" text="Contact Info" />
           <Container TwClassName="flex-row gap-3 items-start">
             <Container TwClassName="w-6 flex justify-center">
@@ -90,6 +42,5 @@ export const TheirProfileAboutTab: React.FC<ProfileTab> = ({
           </Container>
         </Container>
       </Container>
-    </>
   );
 };
