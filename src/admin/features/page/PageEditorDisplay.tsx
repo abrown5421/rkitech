175d08c62da8127f1a156379f9f155cf003f6d78
abrown5421 <augustashtonbrown@gmail.com>
2025-08-20 13:00:19 -0400

    adding in a new exit entrance animation picker component so users can specify the entrance and exit of a page as well as other components

--- src/admin/features/page/PageEditorDisplay.tsx
+++ src/admin/features/page/PageEditorDisplay.tsx
@@ -1,25 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useAppSelector } from '../../../app/hooks';
 import PageContentRenderer from '../../../shared/features/pages/PageContentRenderer';
 import Container from '../../../shared/components/container/Container';
 
 const PageEditorDisplay: React.FC = () => {
-  const localPage = useAppSelector((state) => state.pageEditor.localPageObjectFromDb);
-
-  useEffect(()=>{console.log(localPage)}, [localPage])
+  const localPage = useAppSelector((state) => state.pageEditor);
   
     return (
       <Container TwClassName='flex-col box-border m-4 flex-10 h-[calc(100vh-50px)] overflow-scroll'>
         <Container TwClassName='flex-row text-white mb-4'>buttons</Container>
         <Container
-          TwClassName={`${localPage?.pageBg} flex-col flex-grow`}
+          TwClassName={`${localPage?.localPageObjectFromDb?.pageBg} flex-col flex-grow`}
           animation={{ entranceExit: {
-              entranceAnimation: localPage?.pageEntranceAnimation,
-              exitAnimation: localPage?.pageExitAnimation,
-              isEntering: true,
+              entranceAnimation: localPage?.localPageObjectFromDb?.pageEntranceAnimation,
+              exitAnimation: localPage?.localPageObjectFromDb?.pageExitAnimation,
+              isEntering: localPage.enterExit,
           }}}
         >
-          <PageContentRenderer content={localPage?.content} />
+          <PageContentRenderer content={localPage?.localPageObjectFromDb?.content} />
         </Container>
       </Container>
     );
