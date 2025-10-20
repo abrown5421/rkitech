c96d4b7c55da529347e2d58e391c9b085f1f7959
abrown5421 <augustashtonbrown@gmail.com>
2025-10-20 16:48:05 -0400

    I configure the base api wrong and was pointing to just /api and not localhost:3001/api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Container } from 'rkitech-components';
+import { useGetPagesQuery } from './features/page/pageApi';
 
 const App: React.FC = () => {
+  const { data: pages, isLoading, error } = useGetPagesQuery();
+
+  if (isLoading) return <p>Loading pages...</p>;
+  if (error) return <p>Error loading pages</p>;
 
   return (
     <Container tailwindClasses='w-full h-full'>
-      App
+      {pages?.map((page) => (
+        <div key={page._id}>{page.pageName}</div>
+      ))}
     </Container>
   );
 };
