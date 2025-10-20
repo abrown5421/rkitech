1ab28044ba836e383917c2f00c58935ae50260be
abrown5421 <augustashtonbrown@gmail.com>
2025-10-20 15:56:15 -0400

    adding in a pages api file to interface with the page endpoint of our api

---
+++ frontend/src/features/page/pageApi.ts
@@ -0,0 +1,42 @@
+import { baseApi } from '../../app/api/baseApi';
+import type { IPage } from './pageTypes';
+
+export const pagesApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getPages: build.query<IPage[], void>({
+      query: () => '/pages',
+      providesTags: ['Page'],
+      transformResponse: (response: any) => response.data, 
+    }),
+    createPage: build.mutation<IPage, Partial<IPage>>({
+      query: (body) => ({
+        url: '/pages',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Page'],
+    }),
+    updatePage: build.mutation<IPage, { id: string; data: Partial<IPage> }>({
+      query: ({ id, data }) => ({
+        url: `/pages/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Page'],
+    }),
+    deletePage: build.mutation<void, string>({
+      query: (id) => ({
+        url: `/pages/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Page'],
+    }),
+  }),
+});
+
+export const {
+  useGetPagesQuery,
+  useCreatePageMutation,
+  useUpdatePageMutation,
+  useDeletePageMutation,
+} = pagesApi;
