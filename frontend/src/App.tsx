2b2e135ec733da3a9ec373f37cdd33caad73cb68
abrown5421 <augustashtonbrown@gmail.com>
2025-10-22 14:40:34 -0400

    adding in a placeholder for the page shell and navbar so we can build out our routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Container, Loader } from 'rkitech-components';
 import { usePreloadData } from './hooks/usePreloadData';
+import PageShell from './features/page/PageShell';
 
 const App: React.FC = () => {
   const { loading, error, pages } = usePreloadData();
 
   if (error) return <p>{error}</p>;
+
+  const activePages = pages.filter((page) => page.pageActive);
 
   return (
     <Container tailwindClasses='flex-col w-screen h-screen z-30 relative bg-gray-900'>
@@ -21,14 +25,26 @@ const App: React.FC = () => {
         </Container>
       ) : (
         <Container tailwindClasses="h-full w-full flex-col">
-          {pages.map((page) => (
-            <p key={page._id}>{page.pageName}</p>
-          ))}
+          {/* navbar */}
+          <Container tailwindClasses={`w-full h-[54px] bg-gray-50 relative z-20 shadow-[0_2px_4px_rgba(0,0,0,0.15)]`}>
+            nav here
+          </Container>
+          
+          {/* route tree */}
+          <Routes>
+            {activePages.map((page) => (
+              <Route
+                key={page._id}
+                path={page.pagePath}   
+                element={<PageShell page={page} />}
+              />
+            ))}
+          </Routes>
+          
         </Container>
       )}
     </Container>
   );
 };
-
 
 export default App;
