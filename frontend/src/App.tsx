f5b56abd64aae521fdd987ed4a7a83e5e7c157ef
abrown5421 <augustashtonbrown@gmail.com>
2025-10-23 19:02:17 -0400

    adding in a placeholder for the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
+import Navbar from './features/navbar/Navbar';
 
 const App: React.FC = () => {
 
   return (
-  <div className='flex fkex-row'>
-      <div className='bg-primary text-primary-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-secondary text-secondary-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-accent text-accent-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-success text-success-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-warning text-warning-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-error text-error-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-info text-info-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
-      <div className='bg-neutral text-neutral-content w-20 h-20 m-5 flex items-center justify-center text-3xl font-extrabold'>T</div>
+    <div className='flex flex-col w-screen h-screen'>
+      <Navbar />
     </div>
   );
 };
