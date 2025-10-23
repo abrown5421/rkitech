c8583f613fc5d09ea322b29b4008235b3434c4e5
abrown5421 <augustashtonbrown@gmail.com>
2025-10-23 11:10:13 -0400

    pushing a new initialiuzed project for the frontend with the proper dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import { Container, Loader } from 'rkitech-components';
-import { usePreloadData } from './hooks/usePreloadData';
 
-const App: React.FC = () => {
-  const { loading, error } = usePreloadData();
-
-  if (error) return <p>{error}</p>;
+const Component: React.FC = () => {
 
   return (
-    <Container tailwindClasses='flex-col w-screen h-screen z-30 relative bg-gray-900'>
-      {loading ? (
-        <Container tailwindClasses="h-full w-full justify-center items-center">
-          <Loader
-            show={loading}
-            loaderType='Bars'
-            variant={2}
-            color='amber'
-            intensity={500}
-          />
-        </Container>
-      ) : (
-        <Container tailwindClasses="h-full w-full flex-col">
-          app
-        </Container>
-      )}
-    </Container>
+    <div className=''>hello</div>
   );
 };
-
-export default App;
+export default Component;
