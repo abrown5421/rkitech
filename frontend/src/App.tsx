25153c8c16b45cc8ae5746078a7a1d57489dae26
abrown5421 <augustashtonbrown@gmail.com>
2025-10-23 19:17:53 -0400

    adding in a placeholder element for the footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './features/navbar/Navbar';
+import Footer from './features/footer/Footer';
 
 const App: React.FC = () => {
 
   return (
     <div className='flex flex-col w-screen h-screen'>
       <Navbar />
+      <Footer />
     </div>
   );
 };
