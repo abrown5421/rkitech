96e6c3ce3fe77c2b9ced628a9cbb4faa2b1afda8
august5421 <70552491+august5421@users.noreply.github.com>
2025-10-25 09:45:11 -0400

    adding in a custom page transition animation that is specific to each page

--- frontend/src/features/page/PageShell.tsx
+++ frontend/src/features/page/PageShell.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import type { PageShellProps } from './pageTypes';
 import Footer from '../footer/Footer';
 import Pod from '../../components/pod/Pod';
+import type { EntranceAnimation, ExitAnimation } from '../../components/pod/podTypes';
 
 const PageShell: React.FC<PageShellProps> = ({ page }) => {
 
     return (
         <Pod className='overflow-scroll hide-sb'>
             {page.pageRenderMethod === 'static' ? (
-                <Pod className='page-body p-4'>{page.pageName}</Pod>
+                <Pod 
+                  animationObject={{
+                    entranceAnimation: page.pageEntranceAnimation as EntranceAnimation,
+                    exitAnimation: page.pageExitAnimation as ExitAnimation,
+                    isEntering: true
+                  }} 
+                  className='page-body p-4'
+                >
+                    {page.pageName}
+                </Pod>
             ) : (
                 <Pod>dynamic</Pod>
             )}
