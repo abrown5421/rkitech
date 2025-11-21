4a7cb8cd83220f835263f04a69fd497fe8808175
abrown5421 <augustashtonbrown@gmail.com>
2025-11-21 18:25:20 -0500

    one file left out of the last commit. Just adding it back in

--- frontend/src/features/dynamicForm/dynamicFormSlice.ts
+++ frontend/src/features/dynamicForm/dynamicFormSlice.ts
@@ -13,6 +13,7 @@ const defaultDynamicFormState: DynamicFormProps = {
   mode: 'create',
   editingItem: undefined
 };
+
 const initialState: DynamicFormProps = { ...defaultDynamicFormState };
 
 export const dynamicFormSlice = createSlice({
