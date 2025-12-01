9eee2093d9c2f4d9485bca04ee9128c2ddd06516
abrown5421 <augustashtonbrown@gmail.com>
2025-12-01 14:18:04 -0500

    extending the BoxEditor to use the new redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import BoxEditor from './features/theme/admin/boxEditor/BoxEditor';
+import { useAppSelector } from './store/hooks';
+import BoxEditor from './features/admin/boxEditor/BoxEditor';
 
 const App: React.FC = () => {
+  const boxProps = useAppSelector((state) => state.boxEditor);
 
   return (
     <Box sx={{ display: 'flex', height: '100vh' }}>
       <Box sx={{ flex: 3, bgcolor: 'neutral3.main', color: 'neutral3.content', p: 2 }}>
         <BoxEditor />
       </Box>
-      <Box sx={{ flex: 9, bgcolor: 'neutral.main', color: 'neutral.content', p: 2 }}>
-        B
+      <Box sx={{ flex: 9, bgcolor: 'neutral.main', color: 'neutral.content' }}>
+        <Box {...boxProps}>
+          Live Box Preview
+        </Box>
       </Box>
     </Box>
   );
