21dd075e891388d8d4feca6189f2042d621fd521
abrown5421 <augustashtonbrown@gmail.com>
2025-12-02 18:12:01 -0500

    adding in a placeholder for the admin navbar

---
+++ frontend/src/features/admin/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { Box, Toolbar, Typography, IconButton, useTheme } from '@mui/material';
+import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../../../public/images/Logo.png';
+
+const Navbar: React.FC = () => {
+    const theme = useTheme();
+
+    return (
+        <Toolbar
+            sx={{ 
+                backgroundColor: theme.palette.neutral.main, 
+                color: theme.palette.neutral.content, 
+                display: 'flex', 
+                justifyContent: 'space-between', 
+                px: 2,
+                zIndex: 5,
+                boxShadow: "0 2px 4px rgba(0,0,0,0.15)"
+            }}
+        >
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Box component="img" src={logo} alt="Rkitech Logo" sx={{
+                    height: "100%",
+                    maxHeight: 48,
+                    objectFit: "contain"
+                }}/>
+                <Typography variant='h6' color={theme.palette.primary.main} ml={1} fontFamily="Primary">
+                    Rkitech
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <IconButton 
+                    sx={{ color: theme.palette.neutral.content }}
+                >
+                    <MenuIcon />
+                </IconButton>
+            </Box>
+        </Toolbar>
+    );
+};
+
+export default Navbar;
