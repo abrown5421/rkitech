5fefd1a87cc708094a9e633741a79af7f112f456
abrown5421 <augustashtonbrown@gmail.com>
2025-12-17 12:43:06 -0500

    modifying the box editor to allow for the specifying of animations to containers

--- frontend/src/features/admin/boxEditor/boxEditorTypes.ts
+++ frontend/src/features/admin/boxEditor/boxEditorTypes.ts
@@ -1,16 +1,3 @@
 export interface BoxEditorState {
-  display?: 'flex';
-  flexDirection?: 'row' | 'column';
-  justifyContent?: string;
-  alignItems?: string;
-  gap?: string;
-  m?: string;
-  p?: string;
-  width?: string;
-  height?: string;
-  bgcolor?: string;
-  color?: string;
-  border?: string;
-  borderRadius?: string;
-  boxShadow?: string;
+  animation?: boolean
 }
