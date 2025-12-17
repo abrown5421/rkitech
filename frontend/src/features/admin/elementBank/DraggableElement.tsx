148546f50b26a0e0bbc6a7370b5430a0d39cd3f8
abrown5421 <augustashtonbrown@gmail.com>
2025-12-17 15:05:30 -0500

    adding in a drag and drop feature so that users can drop elements into parent elements to build their own page structure

---
+++ frontend/src/features/admin/elementBank/DraggableElement.tsx
@@ -0,0 +1,28 @@
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
+import { Box } from "@mui/material";
+import React from "react";
+
+interface DraggableElementProps {
+  id: string;
+  children: React.ReactNode;
+}
+
+const DraggableElement: React.FC<DraggableElementProps> = ({ id, children }) => {
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
+    useDraggable({ id });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    opacity: isDragging ? 0.5 : 1,
+    cursor: "grab",
+  };
+
+  return (
+    <Box ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {children}
+    </Box>
+  );
+};
+
+export default DraggableElement;
